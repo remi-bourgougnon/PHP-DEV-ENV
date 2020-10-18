58c169effe712dc43afcf9260170b8566a89674d
Rémi <henri@Henris-MacBook-Pro.local>
2020-10-18 22:23:41 +0200

    first commit

---
+++ website/index.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "db";
+$username = "mysql_user";
+$password = "password";
+$dbname = "mysql_database";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+else{
+    echo "connection successful";
+}
